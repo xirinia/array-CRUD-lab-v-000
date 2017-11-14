4cd303d1fea0b56b477de74d22cf048a93473ef6
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-14 02:54:57 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  var fml = ["Jrg", "Jrgt", "Alss", "Cil"]
+  fml = ["Jrg", "Jrgt", "Alss", "Cil"]
   fml << "Lani"
 end
 
