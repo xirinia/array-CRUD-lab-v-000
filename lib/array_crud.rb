95b5c4f52c94000bccf7ec5a71274f73c3aa3910
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-14 02:49:06 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  ["Jorge", "Jorgito", "Alessandra", "Cielo"]
+  family = ["Jorge", "Jorgito", "Alessandra", "Cielo"]
 end
 
 def add_element_to_end_of_array(array, element)
-
+  family << "Lani"
 end
 
 def add_element_to_start_of_array(array, element)
